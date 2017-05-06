aa94c143a00f5e416ad3cb6ba110fd229ac6feb5
Brodey Newman <brodeynewman@gmail.com>
2017-05-06 15:29:18 -0400

    Finished all state changing methods, added some animation to the products

--- app.js
+++ app.js
@@ -3,31 +3,30 @@ let machine = new Machine();
 let currentDate = new Date();
 currentDate.setDate(currentDate.getDate() + 7);
 
+let pepsi = new Item('Pepsi', '1', '0.1', currentDate, 'A1');
+let coke = new Item('Coke', '1', '0.5', currentDate, 'B3');
+let sprite = new Item('Sprite', '1', '0.5', currentDate, 'C4');
+let fanta = new Item('Fanta', '1', '0.25', currentDate, 'D2');
 
-let pepsi = new Item('Pepsi', '10', '0.5', currentDate, 'A1');
-let coke = new Item('Coke', '5', '0.5', currentDate, 'B3');
-let sprite = new Item('Sprite', '11', '0.5', currentDate, 'C4');
-let fanta = new Item('Fanta', '13', '0.5', currentDate, 'D2');
-
-document.getElementById('five').addEventListener('click', function() {
-  let amount = this.firstElementChild.innerHTML;
-  machine.addChange(amount);
+// these for some reason weren't working with the 'addListeners' method
+document.getElementById('pepsiGrab').addEventListener('click', function() {
+  machine.removeNode(this);
 });
 
-document.getElementById('ten').addEventListener('click', function() {
-  let amount = this.firstElementChild.innerHTML;
-  machine.addChange(amount);
+document.getElementById('cokeGrab').addEventListener('click', function() {
+  machine.removeNode(this);
 });
 
-document.getElementById('twentyFive').addEventListener('click', function() {
-  let amount = this.firstElementChild.innerHTML;
-  machine.addChange(amount);
+/** document.getElementById('fantaGrab').addEventListener('click', function() {
+  machine.removeNode(this);
 });
+
+document.getElementById('spriteGrab').addEventListener('click', function() {
+  machine.removeNode(this);
+}); **/
 
 machine.addListeners(document.querySelectorAll('.button'));
-
+machine.addListeners(document.querySelectorAll('.grab-soda'));
+machine.addListeners(document.querySelectorAll('.coin-outter'));
 machine.loadSodas(pepsi, coke, sprite, fanta);
-
 machine.turnOn();
-
-console.log(machine);
--- machine.js
+++ machine.js
@@ -18,9 +18,7 @@ function Machine() {
 }
 
 Machine.prototype = {
-
   loadSodas: function(...args) {
-
     for (let i = 0; i < args.length; i++) {
       this[args[i].name] = [args[i]];
     }
@@ -42,7 +40,8 @@ Machine.prototype = {
     let total = this.countAllSodas();
     let text = `${total} sodas`;
 
-    document.getElementById('sodaCount').innerHTML = text;
+    this.updateIndividualSodas();
+    (total > 0) ? document.getElementById('sodaCount').innerHTML = text : document.getElementById('sodaCount').innerHTML = 'Empty';
   },
 
   turnOn() {
@@ -52,35 +51,110 @@ Machine.prototype = {
   },
 
   addChange(amount = 0) {
-    this.money+= Number(amount) / 100;
-    document.getElementById('pay').innerHTML = this.money;
+    this.money+= amount;
+
+    // I had to slice the 'money' state because .2 + .2 + .2 in JS is .6000000001 for some reason
+    let slice = this.money.toString().slice(0, 4);
+    document.getElementById('pay').innerHTML = slice;
   },
 
   purchase(node) {
     let self = Object.keys(this);
 
     for (let i = 1; i < self.length; i++) {
-      console.log( this[self[i]][0].price, this.money );
-      if (node.firstElementChild.innerHTML === this[self[i]][0].code && this.money === Number(this[self[i]][0].price)) {
-        this[self[i]][0].amount--;
+      if (node.firstElementChild.innerHTML === this[self[i]][0].code && this.money >= Number(this[self[i]][0].price)) {
+        let name = this[self[i]][0].name;
+
+        if (this[self[i]][0].amount > 0) {
+          let newAmount = this.money - Number(this[self[i]][0].price);
+
+          this[self[i]][0].amount--;
+          this.resetChange(newAmount);
+          this.toggleAnimation(name);
+        } else {
+          alert(`${name} is out of stock!`);
+          this.resetChange(this.money);
+        }
+
         this.update();
-        this.resetChange();
       }
     }
   },
 
-  resetChange() {
-    this.money = 0;
+  resetChange(amount) {
+    this.money = amount;
 
     this.update();
     this.addChange();
   },
 
+  removeFromDom(node) {
+    let lowerNode = node.toLowerCase();
+
+    setTimeout(function() {
+      document.getElementById(lowerNode).style.display = 'none';
+    }, 3000);
+  },
+
+  toggleAnimation(node) {
+    let lowerNode = node.toLowerCase();
+
+    document.getElementById(lowerNode).className = `soda ${lowerNode} ${lowerNode}-drop`;
+    document.getElementById(`${lowerNode}Grab`).className = `grab-soda grab-${lowerNode} ${lowerNode}-slide`;
+  },
+
+  removeNode(node) {
+    node.style.display = 'none';
+    console.log(node);
+  },
+
+  updateIndividualSodas() {
+    let displayBox = document.getElementById('displayBox');
+    let self = Object.keys(this);
+    let displayArr = [];
+
+    displayBox.innerHTML = '';
+
+    for (let i = 1; i < self.length; i++) {
+      displayArr.push(self[i]);
+    }
+
+    for (let i = 1; i < displayArr.length + 1; i++) {
+      let div = document.createElement('div');
+      let amount = this[self[i]][0].amount;
+      let name = this[self[i]][0].name;
+
+      (amount === 0) ? this.removeFromDom(`${name}`) : void 0 ;
+
+      div.className = 'display';
+      div.innerHTML = `${name} (${amount})`;
+      displayBox.append(div);
+    }
+  },
+
   addListeners(nodeList) {
+
     for (let i = 0; i < nodeList.length; i++) {
-      nodeList[i].addEventListener('click', function() {
-        machine.purchase(this);
-      })
+      console.log(nodeList[i].classList.value);
+      switch(nodeList[i].classList.value) {
+        case 'grab-box':
+          nodeList[i].addEventListener('click', function() {
+            alert('hello');
+          });
+          break;
+        case 'button':
+          nodeList[i].addEventListener('click', function() {
+            machine.purchase(this);
+          });
+          break;
+        case 'coin-outter':
+          nodeList[i].addEventListener('click', function() {
+            let amount = Number(this.firstElementChild.innerHTML);
+            machine.addChange(amount);
+          });
+          break;
+      }
+
     }
   }
 }
