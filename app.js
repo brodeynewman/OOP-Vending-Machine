94aa238267ac51e6c33d4fc8dfece1a645cdf108
Brodey Newman <brodeynewman@gmail.com>
2017-05-09 16:00:56 -0400

    Removed expiration

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 let machine = new Machine();
 
-let currentDate = new Date();
-currentDate.setDate(currentDate.getDate() + 7);
-
-let pepsi = new Item('Pepsi', '5', '0.1', currentDate, 'A1');
-let coke = new Item('Coke', '5', '0.5', currentDate, 'B3');
-let sprite = new Item('Sprite', '5', '0.5', currentDate, 'C4');
-let fanta = new Item('Fanta', '5', '0.25', currentDate, 'D2');
+let pepsi = new Item('Pepsi', '5', '0.1', 'A1');
+let coke = new Item('Coke', '5', '0.5', 'B3');
+let sprite = new Item('Sprite', '5', '0.5', 'C4');
+let fanta = new Item('Fanta', '5', '0.25', 'D2');
 
 
 document.getElementById('return').addEventListener('click', function() {
